980ae68a4b8e3fa36b31d95716a00f2dad8056a0
BirdeeHub <robindbor@gmail.com>
2024-04-07 20:55:39 -0700

    updates

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,42 @@ package main
 import (
 	"net/http"
 
+	views "foodbar/views"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"foodbar/views"
 )
 
 func HTML(c echo.Context, code int, cmp templ.Component) error {
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
 	c.Response().Status = code
 	return cmp.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func newExampleRecipeTabData() views.TabData {
+	recipeItem := views.TabItem{
+		ItemName: "Chicken",
+		Ttype:    views.Recipe,
+	}
+	recipeItem1 := views.TabItem{
+		ItemName: "turdSandwich",
+		Ttype:    views.Recipe,
+	}
+	recipeItem2 := views.TabItem{
+		ItemName: "chicken masala",
+		Ttype:    views.Recipe,
+	}
+	recipeItem3 := views.TabItem{
+		ItemName: "tacos caliente",
+		Ttype:    views.Recipe,
+	}
+
+	recipeTabdata := views.TabData{
+		Items: []views.TabItem{recipeItem, recipeItem1, recipeItem2, recipeItem3},
+		Ttype: views.Recipe,
+	}
+
+	return recipeTabdata
 }
 
 func main() {
@@ -22,11 +48,15 @@ func main() {
 		return templ.NewCSSMiddleware(hndl, views.StaticStyles...)
 	}))
 
+	data := views.PageData{
+		TabDatas: []views.TabData{newExampleRecipeTabData()},
+	}
+
 	e.Static("/images", "images")
 
 	e.GET("/", func(c echo.Context) error {
 		e.Logger.Print(c)
-		return HTML(c, http.StatusOK, views.Homepage())
+		return HTML(c, http.StatusOK, views.Homepage(data))
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
