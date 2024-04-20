81fd180cb437ba831c56a67d762fb603266805c9
BirdeeHub <robindbor@gmail.com>
2024-04-20 14:42:18 -0700

    updates

--- FOOdBAR/main.go
+++ FOOdBAR/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 func main() {
-	// TODO: get a much better key from a file
+	// TODO: get a much better key from a file or argument or something
 	signingKey := []byte("secret-passphrase-willitwork")
 	dbpath := os.Getenv("FOOdBAR_STATE")
 	listenOn := ":42069"
