008befdcf297b647b999d62e4538e3d7b5a62f1a
BirdeeHub <robindbor@gmail.com>
2024-04-27 16:22:07 -0700

    updates

--- db/FOOdb.go
+++ db/FOOdb.go
@@ -10,7 +10,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func FillXTabItems(userID uuid.UUID, dbpath string, tbd *viewutils.TabData, number int) error {
+func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.TabType) (*sql.DB, error) {
 	var fooDB string = dbpath
 	if fooDB == "" {
 		fooDB = "/tmp"
@@ -19,40 +19,73 @@ func FillXTabItems(userID uuid.UUID, dbpath string, tbd *viewutils.TabData, numb
 	fooDB = fmt.Sprintf("%s/FOOdBAR/FOOdb.db", dbpath)
 	fooDB, err = foodlib.CreateEmptyFileIfNotExists(fooDB)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	db, err := sql.Open("sqlite3", fooDB)
+
+	switch tt {
+		case viewutils.Recipe:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+		case viewutils.Menu:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+		case viewutils.Pantry:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+		case viewutils.Preplist:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+		case viewutils.Shopping:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+		case viewutils.Earnings:
+			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
+				id TEXT PRIMARY KEY,
+				)`, userID, tt.String()))
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return db, err
+}
+
+/*
+NOTE:
+type TabItem struct {
+	ItemID uuid.UUID `json:"item_id"`
+	Ttype  TabType   `json:"tab_type"`
+	Expanded bool   `json:"expanded"`
+	ItemType string `json:"item_type"`
+}
+	NOTE:
+type SortMethod string
+const (
+	Inactive   SortMethod = ""
+	Descending            = "DESC;"
+	Ascending             = "ASC;"
+	Random                = "RANDOM();"
+	Custom                = "END;"
+
+	// Others can be made with CASE WHEN condition THEN value ELSE value END
+	// when using this syntax, put the CASE WHEN... etc... into the OrderBy key
+	// and then put Custom as the SortMethod
+)
+*/
+func FillXTabItems(userID uuid.UUID, dbpath string, tbd *viewutils.TabData, number int) error {
+	db, err := CreateTabTableIfNotExists(userID, dbpath, tbd.Ttype)
 	defer db.Close()
 	if err != nil {
 		return err
 	}
+	// TODO: fill tbd.Items with X number of items based on tbd.OrderBy: map[string]SortMethod
 
-	switch tbd.Ttype {
-		case viewutils.Recipe:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-		case viewutils.Menu:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-		case viewutils.Pantry:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-		case viewutils.Preplist:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-		case viewutils.Shopping:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-		case viewutils.Earnings:
-			_, err = db.Exec(fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s_%s (
-				id TEXT PRIMARY KEY,
-				)`, userID, tbd.Ttype.String()))
-	}
 
 	return nil
 }
