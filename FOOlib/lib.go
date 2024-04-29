1cb0b5b0efd604cd3e4154ebe1d9ac0aac2bc2b7
BirdeeHub <robindbor@gmail.com>
2024-04-29 02:50:25 -0700

    updates

--- FOOlib/lib.go
+++ FOOlib/lib.go
@@ -70,39 +70,3 @@ func FilterMap[T comparable, V interface{}](f func(T, V) bool, m map[T]V) map[T]
     }
     return ret
 }
-//
-// func FilterPointerSlice[T interface{}](f func(*T) bool, list []*T) ([]*T) {
-//     var ret []*T
-//     for _, item := range list {
-//         if f(item) {
-//             ret = append(ret, item)
-//         }
-//     }
-//     return ret
-// }
-//
-// func MapPointerSlice[T interface{}, V interface{}](f func(*T) *V, list []*T) ([]*V) {
-//     var ret []*V
-//     for _, item := range list {
-//         ret = append(ret, f(item))
-//     }
-//     return ret
-// }
-//
-// func MapPointerMap[T any, V any, R any](f func(*T, *V) *R, m map[*T]*V) map[*T]*R {
-//     ret := make(map[*T]*R)
-//     for k, v := range m {
-//         ret[k] = f(k, v)
-//     }
-//     return ret
-// }
-//
-// func FilterPointerMap[T any, V any](f func(*T, *V) bool, m map[*T]*V) map[*T]*V {
-//     ret := make(map[*T]*V)
-//     for k, v := range m {
-//         if f(k, v) {
-//             ret[k] = v
-//         }
-//     }
-//     return ret
-// }
