a58766e44358ee8f18ea4ddbdb14614ff89944f6
BirdeeHub <robindbor@gmail.com>
2024-05-01 03:09:44 -0700

    updates

--- db/FOOdb.go
+++ db/FOOdb.go
@@ -26,8 +26,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 
 	var createTable string
 	switch tt {
-		case viewutils.Recipe:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Recipe:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -38,8 +38,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				ingredients TEXT,
 				instructions TEXT,
 				)`
-		case viewutils.Menu:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Menu:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -48,8 +48,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				recipe TEXT,
 				number INTEGER,
 				)`
-		case viewutils.Pantry:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Pantry:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -58,8 +58,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				dietary TEXT,
 				amount TEXT,
 				)`
-		case viewutils.Customer:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Customer:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -69,8 +69,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				name TEXT,
 				dietary TEXT,
 				)`
-		case viewutils.Events:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Events:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -80,8 +80,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				location TEXT,
 				customer TEXT,
 				)`
-		case viewutils.Preplist:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Preplist:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -89,8 +89,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				event_id TEXT,
 				menu_id TEXT,
 				)`
-		case viewutils.Shopping:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Shopping:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -101,8 +101,8 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				amount TEXT,
 				units TEXT,
 				)`
-		case viewutils.Earnings:
-			createTable = `CREATE TABLE IF NOT EXISTS ?_? (
+	case viewutils.Earnings:
+		createTable = `CREATE TABLE IF NOT EXISTS ?_? (
 				id TEXT PRIMARY KEY,
 				created_at TEXT,
 				last_modified TEXT,
@@ -110,17 +110,17 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 				event_id TEXT,
 				menu_id TEXT,
 				)`
-		default:
-			return nil, errors.New("invalid tab type")
+	default:
+		return nil, errors.New("invalid tab type")
 	}
 	createStmt, err := db.Prepare(createTable)
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
 	defer createStmt.Close()
 	_, err = createStmt.Exec(userID, tt.String())
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
 	err = makeAuditTriggers(db, userID, tt)
 	if err != nil {
@@ -131,60 +131,59 @@ func CreateTabTableIfNotExists(userID uuid.UUID, dbpath string, tt viewutils.Tab
 
 func makeAuditTriggers(db *sql.DB, userID uuid.UUID, tt viewutils.TabType) error {
 
-  updateTrigger := `
-    CREATE TRIGGER IF NOT EXISTS update_?_?_audit 
-    AFTER UPDATE OR DELETE ON ?_?
-    FOR EACH ROW
-    BEGIN
-      UPDATE ?_?
-      SET last_modified = CURRENT_TIMESTAMP
-      WHERE id = OLD.id;
-    END;`
+	updateTrigger := `CREATE TRIGGER IF NOT EXISTS update_?_?_audit 
+		AFTER UPDATE OR DELETE ON ?_?
+		FOR EACH ROW
+		BEGIN
+			UPDATE ?_?
+			SET last_modified = CURRENT_TIMESTAMP
+			WHERE id = OLD.id;
+		END;`
 
-  insertTrigger := `
-    CREATE TRIGGER IF NOT EXISTS add_created_?_?_audit
-    AFTER INSERT ON ?_?
-    FOR EACH ROW  
-    BEGIN
-      UPDATE ?_?
-      SET created_at = CURRENT_TIMESTAMP
-      WHERE id = OLD.id;
-    END;`
+	insertTrigger := `CREATE TRIGGER IF NOT EXISTS add_created_?_?_audit
+		AFTER INSERT ON ?_?
+		FOR EACH ROW  
+		BEGIN
+			UPDATE ?_?
+			SET created_at = CURRENT_TIMESTAMP
+			WHERE id = OLD.id;
+		END;`
 
-  updateStmt, err := db.Prepare(updateTrigger)
-  if err != nil {
-    return err
-  }
-  defer updateStmt.Close()
+	updateStmt, err := db.Prepare(updateTrigger)
+	if err != nil {
+		return err
+	}
+	defer updateStmt.Close()
 
-  _, err = updateStmt.Exec(userID, tt.String(), userID, tt.String(), userID, tt.String())
-  if err != nil {
-    return err
-  }
+	_, err = updateStmt.Exec(userID, tt.String(), userID, tt.String(), userID, tt.String())
+	if err != nil {
+		return err
+	}
 
-  insertStmt, err := db.Prepare(insertTrigger)
-  if err != nil {
-    return err
-  }
-  defer insertStmt.Close()
+	insertStmt, err := db.Prepare(insertTrigger)
+	if err != nil {
+		return err
+	}
+	defer insertStmt.Close()
 
-  _, err = insertStmt.Exec(userID, tt.String(), userID, tt.String(), userID, tt.String())
-  return err
+	_, err = insertStmt.Exec(userID, tt.String(), userID, tt.String(), userID, tt.String())
+	return err
 }
-
-
-
 
 /*
 NOTE:
-type TabItem struct {
-	ItemID uuid.UUID `json:"item_id"`
-	Ttype  TabType   `json:"tab_type"`
-	Expanded bool   `json:"expanded"`
-}
+
+	type TabItem struct {
+		ItemID uuid.UUID `json:"item_id"`
+		Ttype  TabType   `json:"tab_type"`
+		Expanded bool   `json:"expanded"`
+	}
+
 	NOTE:
+
 type SortMethod string
 const (
+
 	Inactive   SortMethod = ""
 	Descending            = "DESC;"
 	Ascending             = "ASC;"
@@ -194,6 +193,7 @@ const (
 	// Others can be made with CASE WHEN condition THEN value ELSE value END
 	// when using this syntax, put the CASE WHEN... etc... into the OrderBy key
 	// and then put Custom as the SortMethod
+
 )
 */
 func FillXTabItems(userID uuid.UUID, dbpath string, tbd *viewutils.TabData, number int) error {
@@ -204,7 +204,6 @@ func FillXTabItems(userID uuid.UUID, dbpath string, tbd *viewutils.TabData, numb
 	}
 	// TODO: fill tbd.Items with X number of items based on tbd.OrderBy: map[string]SortMethod
 	// where the key string is a column name
-
 
 	return nil
 }
