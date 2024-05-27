8c68b898913d97abbdf53fafcfb7cddda142c9a2
BirdeeHub <robindbor@gmail.com>
2024-05-27 07:28:57 -0700

    updates

---
+++ FOOstatic/events.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.body.addEventListener("htmx:beforeSwap", function(evt) {
+        if (evt.detail.xhr.status === 422) {
+            evt.detail.shouldSwap = true;
+            evt.detail.isError = false;
+        }
+    });
+})
+document.addEventListener('htmx:configRequest', (event) => {
+    function generateUniqueID() {
+        var id = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
+        sessionStorage.setItem('tab_id', id);
+        return id;
+    }
+    var tabId = sessionStorage.getItem('tab_id') || generateUniqueID();
+    event.detail.headers['tab_id'] = tabId;
+});
+document.addEventListener('htmx:afterRequest', (event) => {
+    const tabIdHeader = event.detail.xhr.getResponseHeader('tab_id');
+    if (tabIdHeader) {
+        sessionStorage.setItem('tab_id', tabIdHeader);
+    }
+});
