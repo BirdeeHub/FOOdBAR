740bde838b8f4208d637352273ff0f315185ad50
BirdeeHub <robindbor@gmail.com>
2024-05-29 02:14:48 -0700

    updates

--- srvapi/flipRoutes.go
+++ srvapi/flipRoutes.go
@@ -11,52 +11,9 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func SetupModalAPIroutes(e *echo.Group) error {
+func SetupFlipAPIroutes(e *echo.Group) error {
 
-	e.POST("/api/submitItemInfo/:type/:itemID", func(c echo.Context) error {
-		// TODO: This should refresh the tab it was added to so it can get new values.
-		pageData, err := db.GetPageData(c)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusUnauthorized, err)
-		}
-		tt := foodlib.String2TabType(c.Param("type"))
-		if tt == foodlib.Invalid {
-			return echo.NewHTTPError(http.StatusUnprocessableEntity, errors.New("invalid tab type"))
-		}
-		itemID, err := uuid.Parse(c.Param("itemID"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusUnprocessableEntity, errors.New("itemID is not a valid UUID"))
-		}
-		td := pageData.GetTabDataByType(tt)
-		c.Logger().Print(td)
-		ti := td.GetTabItem(itemID)
-		c.Logger().Print(ti)
-		switch tt {
-		case foodlib.Recipe:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Pantry:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Menu:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Preplist:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Shopping:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Events:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Customer:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		case foodlib.Earnings:
-			err = db.SubmitPantryItem(c, pageData, td, ti)
-		}
-		if err != nil {
-			c.Logger().Print(err)
-			return HTML(c, http.StatusUnprocessableEntity, tabviews.OOBsendBackSubmitStatus(tt, "", err))
-		}
-		return HTML(c, http.StatusUnprocessableEntity, tabviews.OOBsendBackSubmitStatus(tt, "Item Saved Successfully!", nil))
-	})
-
-	e.GET("/api/modalGetNewField/:type/:itemID/:field", func(c echo.Context) error {
+	e.GET("/api/flipGetNewField/:type/:itemID/:field", func(c echo.Context) error {
 		pageData, err := db.GetPageData(c)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, err)
@@ -76,7 +33,7 @@ func SetupModalAPIroutes(e *echo.Group) error {
 		return HTML(c, http.StatusOK, tabviews.OOBExtraField(c.Param("field")))
 	})
 
-	e.GET("/api/itemEditModal/open/:type/:itemID", func(c echo.Context) error {
+	e.GET("/api/itemEditFlip/open/:type/:itemID", func(c echo.Context) error {
 		pageData, err := db.GetPageData(c)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, err)
@@ -100,7 +57,7 @@ func SetupModalAPIroutes(e *echo.Group) error {
 		return HTML(c, http.StatusOK, tabviews.ItemEditModal(tabviews.RenderSubmissionContent(pageData, item, nil)))
 	})
 
-	e.GET("/api/itemCreateModal/open/:type", func(c echo.Context) error {
+	e.GET("/api/itemCreateFlip/open/:type", func(c echo.Context) error {
 		pageData, err := db.GetPageData(c)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, err)
