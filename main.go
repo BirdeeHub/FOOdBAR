d4a3cf7d6bf0e9145dc8ac096b64046a154a56ea
BirdeeHub <robindbor@gmail.com>
2024-05-31 08:15:12 -0700

    updates

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var staticFiles embed.FS
 
 func main() {
 	var signingKeyPath string
-	flag.StringVar(&signingKeyPath, "keypath", "", "key file to use for signed cookies (overrides FOOdBAR_SIGNING_KEY env var)")
+	flag.StringVar(&signingKeyPath, "keypath", os.Getenv("FOOdBAR_SIGNING_KEY"), "key file to use for signed cookies (overrides FOOdBAR_SIGNING_KEY env var)")
 	var dbpath string
 	flag.StringVar(&dbpath, "dbpath", os.Getenv("FOOdBAR_STATE"), "path to database directory (overrides FOOdBAR_STATE env var), defaults to /tmp or windows temp folder if not set")
 	var port int
@@ -45,14 +45,9 @@ func main() {
 		err = fmt.Errorf("keypath not set")
 	}
 	if err != nil {
-		keystring := os.Getenv("FOOdBAR_SIGNING_KEY")
-		if keystring != "" {
-			signingKey = []byte(os.Getenv(keystring))
-		} else {
-			signingKey = []byte("secret-passphrase-willitwork")
-			fmt.Println("Error: ", err)
-			fmt.Println("Danger: using default signing key. Use -keypath or FOOdBAR_SIGNING_KEY environment var to set it.")
-		}
+		signingKey = []byte("secret-passphrase-willitwork")
+		fmt.Println("Error: ", err)
+		fmt.Println("Danger: using default signing key. Use -keypath or FOOdBAR_SIGNING_KEY environment var to set it.")
 	}
 	srvapi.InitServer(signingKey, listenOn, staticFiles)
 }
