8c99e1d8949ac5bc0f363b67377e796790c88d84
BirdeeHub <robindbor@gmail.com>
2024-05-31 10:53:37 -0700

    updates

--- default.nix
+++ default.nix
@@ -26,6 +26,9 @@ buildGoApplication {
     targetStaticDir=$TEMPDIR/$sourceRoot/static
     mkdir -p $targetStaticDir
     tailwindcss -o $targetStaticDir/tailwind.css -c ${./tailwind.config.js} --minify
+    cp ${inputs.htmx}/dist/htmx.min.js $targetStaticDir
+    cp ${inputs.hyperscript}/dist/_hyperscript.min.js $targetStaticDir
+    # TODO: Figure out how to get browser to unpack this.
     gzip -c ${inputs.htmx}/dist/htmx.min.js > $targetStaticDir/htmx.min.js.gz
     gzip -c ${inputs.hyperscript}/dist/_hyperscript.min.js > $targetStaticDir/_hyperscript.min.js.gz
   '';
