65ea111664cb589b5d0a1bd12f7bf3fd8236d15e
BirdeeHub <robindbor@gmail.com>
2024-06-01 01:07:13 -0700

    README

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 //go:embed static/*
@@ -34,14 +35,27 @@ func main() {
 		foodlib.HyperscriptPath = "/static/_hyperscript.min.js"
 	}
 
+	listenAddress := os.Getenv("FOOdBAR_ADDRESS")
+	if listenAddress == "" {
+		listenAddress = "localhost"
+	}
+
+	listenPort := 42069
+	if listenPortStr := os.Getenv("FOOdBAR_PORT"); listenPortStr != "" {
+		v, err := strconv.Atoi(listenPortStr)
+		if err == nil {
+			listenPort = v
+		}
+	}
+
 	var signingKeyPath string
 	flag.StringVar(&signingKeyPath, "keypath", os.Getenv("FOOdBAR_SIGNING_KEY"), "key file to use for signed cookies (overrides FOOdBAR_SIGNING_KEY env var)")
 	var dbpath string
 	flag.StringVar(&dbpath, "dbpath", os.Getenv("FOOdBAR_STATE"), "path to database directory (overrides FOOdBAR_STATE env var), defaults to /tmp or windows temp folder if not set")
 	var port int
-	flag.IntVar(&port, "port", 42069, "port to listen on")
+	flag.IntVar(&port, "port", listenPort, "port to listen on")
 	var ip string
-	flag.StringVar(&ip, "ip", "localhost", "IP address to bind to")
+	flag.StringVar(&ip, "ip", listenAddress, "IP address to bind to")
 	flag.Parse()
 
 	listenOn := fmt.Sprintf("%s:%d", ip, port)
